abb9247b6af069ee0bfc91056da6d28bab5c5de2
ian <ian.escarro@gmail.com>
2023-10-21 10:47:09 +0800

    more update

--- test.php
+++ test.php
@@ -13,3 +13,13 @@ $traxion->init($url, $secret_key, true);
 
 $r = $traxion->account_number($username, $password);
 print_pre($r);
+
+$amount = 1;
+$r = $traxion->qr_generate_merchant($username, $password, $amount);
+$qr = $r->data->rawString;
+print_pre($r);
+
+echo "<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$qr&choe=UTF-8'><br>";
+
+$r = $traxion->bank_transfer_qr_check($username, $password, $qr);
+print_pre($r);
