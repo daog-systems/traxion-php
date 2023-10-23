ff530aed2b0df28e36187e24f81c2b611868ed61
ian <ian.escarro@gmail.com>
2023-10-24 06:39:10 +0800

    more tests

--- test.php
+++ test.php
@@ -26,10 +26,10 @@ $secretKey = '';
 $traxion = new Traxion();
 $traxion->init($url, $secretKey, true);
 
-// $r = $traxion->account_number($username, $password); // OK
+// $r = $traxion->account_number($username, $password);
 // print_pre($r, '<span class="text-success">Output</span>');
 
-// $r = $traxion->wallet($username, $password); // OK
+// $r = $traxion->wallet($username, $password);
 // print_pre($r, '<span class="text-success">Output</span>');
 
 // $batchIdentifier = "batch-may-2-1";
@@ -46,22 +46,24 @@ $traxion->init($url, $secretKey, true);
 // $r = $traxion->funds_transfer_bulk($username, $password, $batchIdentifier, $institutionID, $accountNumber, $merchantReferenceNumber, $amount, $recipientName, $mobileNumber, $tag, $notifyRecipient, $purpose, $message);
 // print_pre($r, '<span class="text-success">Output</span>');
 
-// $amount = 1;
-// $r = $traxion->qr_generate_merchant($username, $password, $amount); // OK
+// $amount = 10;
+// $r = $traxion->qr_generate($username, $password, $amount);
+// print_pre($r, '<span class="text-success">Output</span>');
+
+// $amount = 10;
+// $r = $traxion->qr_generate_merchant($username, $password, $amount);
 // $qr = $r->data->rawString;
 // print_pre($r, '<span class="text-success">Output</span>');
 // echo "<img src='https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$qr&choe=UTF-8'><br>";
 
-// $qr = '00020101021228780011ph.ppmi.p2m0111TRXPPHM2XXX03171016202300004685604161001173251356404050300052046016530360854041.005802PH5907OLYMPUS6010PASIG CITY623505062110000014D00KF0QIXBSQHH0803***88490012ph.ppmi.qrph0129006395677194150211BCPIPHSSXXX63048A2D';
-// $r = $traxion->bank_transfer_qr_check($username, $password, $qr); // OK
+// $r = $traxion->bank_transfer_qr_check($username, $password, $qr);
 // print_pre($r, '<span class="text-success">Output</span>');
 
-$qr = '00020101021228780011ph.ppmi.p2m0111TRXPPHM2XXX03171016202300004685604161001173251356404050300052046016530360854041.005802PH5907OLYMPUS6010PASIG CITY623505062110000014D00KF0QIXBSQHH0803***88490012ph.ppmi.qrph0129006395677194150211BCPIPHSSXXX63048A2D';
-$merchantReferenceNumber = ''; // TODO: Where to get this?
-$amount = 1;
-$description = '';
-$r = $traxion->bank_transfer_qr($username, $password, $qr, $merchantReferenceNumber, $amount, $description);
-print_pre($r, '<span class="text-success">Output</span>');
+// $merchantReferenceNumber = ''; // TODO: Where to get this?
+// $amount = 10;
+// $description = '';
+// $r = $traxion->bank_transfer_qr($username, $password, $qr, $merchantReferenceNumber, $amount, $description);
+// print_pre($r, '<span class="text-success">Output</span>');
 
 // $referenceNumber = '211000'; // TODO: Where to get this?
 // $r = $traxion->details_merchant_reference($username, $password, $referenceNumber);
@@ -77,18 +79,28 @@ print_pre($r, '<span class="text-success">Output</span>');
 
 // $r = $traxion->payment_categories($username, $password);
 // print_pre($r, '<span class="text-success">Output</span>');
-
+//
 // $categoryId = 3;
 // $r = $traxion->payment_categories_methods($username, $password, $categoryId);
 // print_pre($r, '<span class="text-success">Output</span>');
 
 // $amount = 10;
-// $r = $traxion->cash_in_gcash($username, $password, $amount);
+// $paymentMethod = 131; // GCASH
+// $paymentCategoryID = 3;
+// $institutionID = 36837;
+// $aggregatorID = 11;
+// $r = $traxion->cash_in_gcash($username, $password, $amount, $paymentMethod, $paymentCategoryID, $institutionID, $aggregatorID);
 // print_pre($r, '<span class="text-success">Output</span>');
 
 // $amount = 10;
-// $r = $traxion->cash_in_gcash_fee($username, $password, $amount);
+// $institutionId = 36837;
+// $aggregatorId = 11;
+// $r = $traxion->cash_in_gcash_fee($username, $password, $amount, $institutionId, $aggregatorId);
 // print_pre($r, '<span class="text-success">Output</span>');
 
-// $r = $traxion->transaction_history($username, $password); // OK
+// $referenceNumber = 'TXN4ba8bc3e64d342c5';
+// $r = $traxion->details_merchant_reference($username, $password, $referenceNumber);
+// print_pre($r);
+
+// $r = $traxion->transaction_history($username, $password);
 // print_pre($r, '<span class="text-success">Output</span>');
